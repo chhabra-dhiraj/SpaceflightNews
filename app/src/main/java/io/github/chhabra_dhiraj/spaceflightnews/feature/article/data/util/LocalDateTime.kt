5bdcce619ca18a2d10dc911bf7fa2281d16e2201
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-08-11 23:39:23 +0530

    fix: improve exception handling of localdatetime helper extension fun

--- app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/data/util/LocalDateTime.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/data/util/LocalDateTime.kt
@@ -2,11 +2,10 @@ package io.github.chhabra_dhiraj.spaceflightnews.feature.article.data.util
 
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.time.format.DateTimeParseException
 
 fun String.getLocalDateTime() = try {
     LocalDateTime.parse(this, DateTimeFormatter.ISO_DATE_TIME)
-} catch (e: DateTimeParseException) {
+} catch (e: Exception) {
     e.printStackTrace()
     // To avoid app crash, and instead, use an appropriate placeholder (e.g. "Time Unknown")
     null

2af0e92eefebdad42ec88f27ebdaf865b22c2fdc
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-08-13 00:06:19 +0530

    feat: add Article Image placeholder for loading and error

--- app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/presentation/articlelist/component/ArticleImage.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/presentation/articlelist/component/ArticleImage.kt
@@ -1,18 +1,36 @@
 package io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import coil.compose.AsyncImage
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.vectorResource
+import coil.compose.SubcomposeAsyncImage
+import io.github.chhabra_dhiraj.spaceflightnews.R
 
 @Composable
 fun ArticleImage(
     imageUrl: String,
     modifier: Modifier = Modifier
 ) {
-    // TODO: check if this is supported. And, add error placeholder
-    AsyncImage(
+    SubcomposeAsyncImage(
         model = imageUrl,
         contentDescription = null,
+        loading = {
+            CircularProgressIndicator(
+                color = MaterialTheme.colorScheme.primary,
+                modifier = Modifier.scale(0.3f)
+            )
+        },
+        error = {
+            Image(
+                imageVector = ImageVector.vectorResource(id = R.drawable.baseline_newspaper_24),
+                contentDescription = null
+            )
+        },
         modifier = modifier
     )
 }

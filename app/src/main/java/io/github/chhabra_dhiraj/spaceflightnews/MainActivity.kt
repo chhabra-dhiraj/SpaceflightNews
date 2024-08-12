2726eb5a0afd5310b9417414b6f074ac52eaf753
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-08-12 23:32:53 +0530

    refactor: mainactivity viewmodel cleanup

--- app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/MainActivity.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -15,8 +14,6 @@ import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.ui.
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    private val viewModel: ArticleListViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

7e2c642c58bb6604cac00246c5da95d56d1d5467
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-08-13 01:51:20 +0530

    refactor: extract FAB to a separate component - ArticleListRefreshFab

--- app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/presentation/articlelist/ArticleListScreen.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/presentation/articlelist/ArticleListScreen.kt
@@ -4,12 +4,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Refresh
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
-import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -17,9 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
 import io.github.chhabra_dhiraj.spaceflightnews.R
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.component.ArticleList
+import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.component.ArticleListRefreshFab
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.component.EmptyArticleListState
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.component.ErrorArticleListState
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.component.LoadingArticleListState
@@ -41,18 +36,13 @@ fun ArticleListScreen(
                     )
                 })
         },
+        // TODO: Handle padding overlap for thisstatus
         floatingActionButton = {
-            FloatingActionButton(
-                onClick = {
+            ArticleListRefreshFab(
+                onRefresh = {
                     onEvent(ArticleListEvent.OnRefreshArticleList)
-                },
-                shape = RoundedCornerShape(20.dp)
-            ) {
-                Icon(
-                    imageVector = Icons.Rounded.Refresh,
-                    contentDescription = stringResource(R.string.cd_refresh_article_list)
-                )
-            }
+                }
+            )
         },
         modifier = Modifier.fillMaxSize()
     ) { contentPadding ->

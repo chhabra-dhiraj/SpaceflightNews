d188f847bab06e888c485f82ef8321d87783c585
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-08-14 01:44:13 +0530

    refactor: add type safe navigation compose

--- app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/Navigation.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/Navigation.kt
@@ -6,21 +6,20 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.Immutable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import androidx.navigation.navArgument
+import androidx.navigation.toRoute
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articledetail.ArticleDetailEvent
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articledetail.ArticleDetailScreen
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articledetail.ArticleDetailViewModel
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.ArticleListScreen
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.articlelist.ArticleListViewModel
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.presentation.util.rememberFlowWithLifecycle
+import kotlinx.serialization.Serializable
 
 @Immutable
 sealed class NavigationEvent {
@@ -32,8 +31,11 @@ sealed class NavigationEvent {
 fun MainNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "article_list_screen") {
-        composable("article_list_screen") {
+    NavHost(
+        navController = navController,
+        startDestination = ArticleList
+    ) {
+        composable<ArticleList> {
             val viewModel = hiltViewModel<ArticleListViewModel>()
             val state by viewModel.state.collectAsStateWithLifecycle()
             val navigationEvent = rememberFlowWithLifecycle(viewModel.navigationEvent)
@@ -43,7 +45,9 @@ fun MainNavigation() {
                     when (event) {
                         is NavigationEvent.NavigateToArticle -> {
                             navController.navigate(
-                                "article_detail_screen/${event.articleId}"
+                                ArticleDetail(
+                                    articleId = event.articleId
+                                )
                             )
                         }
 
@@ -57,59 +61,52 @@ fun MainNavigation() {
                 onEvent = viewModel::onEvent
             )
         }
-        composable(
-            "article_detail_screen/{articleId}",
-            arguments = listOf(
-                navArgument("articleId") {
-                    type = NavType.IntType
-                }
-            )
-        ) {
-            val articleId = remember {
-                it.arguments?.getInt("articleId")
-            }
+        composable<ArticleDetail> {
+            val args = it.toRoute<ArticleDetail>()
+            val context = LocalContext.current
+            val viewModel = hiltViewModel<ArticleDetailViewModel>()
+            val state by viewModel.state.collectAsStateWithLifecycle()
+            val navigationEvent = rememberFlowWithLifecycle(viewModel.navigationEvent)
+            val urlIntentEvent = rememberFlowWithLifecycle(viewModel.urlIntentEvent)
 
-            // TODO: Handle the case where articleId is null
-            articleId?.let {
-                val context = LocalContext.current
-                val viewModel = hiltViewModel<ArticleDetailViewModel>()
-                val state by viewModel.state.collectAsStateWithLifecycle()
-                val navigationEvent = rememberFlowWithLifecycle(viewModel.navigationEvent)
-                val urlIntentEvent = rememberFlowWithLifecycle(viewModel.urlIntentEvent)
-
-                LaunchedEffect(navigationEvent) {
-                    navigationEvent.collect { event ->
-                        when (event) {
-                            is NavigationEvent.NavigateBack -> {
-                                navController.popBackStack()
-                            }
-
-                            else -> Unit
+            LaunchedEffect(navigationEvent) {
+                navigationEvent.collect { event ->
+                    when (event) {
+                        is NavigationEvent.NavigateBack -> {
+                            navController.popBackStack()
                         }
+
+                        else -> Unit
                     }
                 }
-
-                LaunchedEffect(urlIntentEvent) {
-                    urlIntentEvent.collect { url ->
-                        context.startActivity(
-                            Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                        )
-                    }
-                }
-
-                // TODO: Find a solution for this
-                viewModel.onEvent(
-                    ArticleDetailEvent
-                        .LoadArticleDetail(
-                            articleId = it
-                        )
-                )
-
-                ArticleDetailScreen(
-                    state = state,
-                    onEvent = viewModel::onEvent
-                )
             }
+
+            LaunchedEffect(urlIntentEvent) {
+                urlIntentEvent.collect { url ->
+                    Intent(Intent.ACTION_VIEW, Uri.parse(url)).also { intent ->
+                        context.startActivity(intent)
+                    }
+                }
+            }
+
+            // TODO: Find a solution for this
+            viewModel.onEvent(
+                ArticleDetailEvent
+                    .LoadArticleDetail(
+                        articleId = args.articleId
+                    )
+            )
+
+            ArticleDetailScreen(
+                state = state,
+                onEvent = viewModel::onEvent
+            )
         }
     }
 }
+
+@Serializable
+object ArticleList
+
+@Serializable
+data class ArticleDetail(val articleId: Int)

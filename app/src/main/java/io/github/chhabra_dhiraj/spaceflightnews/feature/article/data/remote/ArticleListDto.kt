a51872c70cd82f93560a15b325c2144e076d9a3f
Dhiraj Chhabra <dhiraj.chhabra.g@gmail.com>
2024-08-13 13:42:05 +0530

    feat: add data layer for fetching a single Article

--- app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/data/remote/ArticleListDto.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/data/remote/ArticleListDto.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class ArticlesDto(
+data class ArticleListDto(
     @SerialName("results")
     val articles: List<ArticleDto>
 )
--- app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/data/repository/ArticleRepositoryImpl.kt
+++ app/src/main/java/io/github/chhabra_dhiraj/spaceflightnews/feature/article/data/repository/ArticleRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package io.github.chhabra_dhiraj.spaceflightnews.feature.article.data.repository
 
+import io.github.chhabra_dhiraj.spaceflightnews.feature.article.data.mapper.toArticle
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.data.mapper.toArticleList
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.data.remote.ArticleApi
 import io.github.chhabra_dhiraj.spaceflightnews.feature.article.domain.model.Article
@@ -10,13 +11,29 @@ import javax.inject.Inject
 class ArticleRepositoryImpl @Inject constructor(
     private val api: ArticleApi
 ) : ArticleRepository {
-    override suspend fun getArticles(): Resource<List<Article>> {
+
+    override suspend fun getArticleList(): Resource<List<Article>> {
         return try {
             Resource.Success(
-                data = api.getArticles().toArticleList()
+                data = api.getArticleList().toArticleList()
             )
         } catch (e: Exception) {
             e.printStackTrace()
+            // TODO: extract string to a string resource
+            Resource.Error(message = e.message ?: "An unknown error occurred!")
+        }
+    }
+
+    override suspend fun getArticle(articleId: Int): Resource<Article> {
+        return try {
+            Resource.Success(
+                data = api.getArticle(
+                    articleId = articleId
+                ).toArticle()
+            )
+        } catch (e: Exception) {
+            e.printStackTrace()
+            // TODO: extract string to a string resource
             Resource.Error(message = e.message ?: "An unknown error occurred!")
         }
     }
